114bd1c630ab05e0fe59dbc44195fecd2004bdde
mohammedraziqrehman <mohammedraziqrehman7865@gmail.com>
2023-10-29 13:34:27 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project is for demonstration of ml n mlop orchestration',
+    author='razi',
+    license='MIT',
+)
